8a9f911f78f1c7f4ed71b12e2b3c246c97a51ec2
Kathryn <keb97@cornell.edu>
2013-08-20 22:25:27 -0700

    get public index out of git

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,9 @@
 class Photo < ActiveRecord::Base
   attr_accessible :title, :image
   mount_uploader :image, ImageUploader
+
+  after_save :enqueue_image
+
   def image_name
     File.basename(image.path || image.filename) if image
   end
